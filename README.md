9808d6fe2f853f6146451dce4b70f2b4b03f0fc6
Dhruv-The-Onlne-Coder <141032346+Dhruv-The-Onlne-Coder@users.noreply.github.com>
2023-10-23 19:26:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# 2048
+# 2048 Game #
+
+This is a simple implementation of the popular 2048 game using Python and the tkinter library for the graphical user interface.
+
+## Features
+
+- Fullscreen mode for an immersive gaming experience.
+- Beautiful colors and fonts to enhance the visual appeal.
+- Keyboard controls for moving and merging tiles.
+- Score tracking to keep track of your progress.
+- Game over conditions to determine if you win or lose.
+
+## How to Play
+
+1. Run the game.
+2. Use arrow keys (Left, Right, Up, Down) to move the tiles.
+3. Combine tiles with the same number to reach the 2048 tile.
+4. Keep playing and aim for the highest score!
+
+## Installation
+
+1. Clone this repository:
+
+   ```bash
+   git clone https://github.com/your-username/2048-game.git
---
+++ colors.py
@@ -0,0 +1,50 @@
+GRID_COLOR = "#FFD700"  # Gold
+EMPTY_CELL_COLOR = "#D3D3D3"  # Light Gray
+SCORE_LABEL_FONT = ("Arial", 24, "bold")
+SCORE_FONT = ("Arial", 36, "bold")
+GAME_OVER_FONT = ("Arial", 48, "bold")
+GAME_OVER_FONT_COLOR = "#FF4500"  # Orange-Red
+WINNER_BG = "#FFD700"  # Gold
+LOSER_BG = "#8B0000"  # Dark Red
+
+CELL_COLORS = {
+    2: "#F0E68C",    # Khaki
+    4: "#FFD700",    # Gold
+    8: "#FFA500",    # Orange
+    16: "#FF8C00",   # Dark Orange
+    32: "#FF6347",   # Tomato
+    64: "#FF4500",   # Orange-Red
+    128: "#FFD700",  # Gold
+    256: "#FFD700",  # Gold
+    512: "#FFD700",  # Gold
+    1024: "#FFD700",  # Gold
+    2048: "#FFD700"  # Gold
+}
+
+CELL_NUMBER_COLORS = {
+    2: "#2F4F4F",     # Dark Slate Gray
+    4: "#2F4F4F",     # Dark Slate Gray
+    8: "#FFFFFF",     # White
+    16: "#FFFFFF",    # White
+    32: "#FFFFFF",    # White
+    64: "#FFFFFF",    # White
+    128: "#FFFFFF",   # White
+    256: "#FFFFFF",   # White
+    512: "#FFFFFF",   # White
+    1024: "#FFFFFF",  # White
+    2048: "#FFFFFF"   # White
+}
+
+CELL_NUMBER_FONTS = {
+    2: ("Arial", 55, "bold"),
+    4: ("Arial", 55, "bold"),
+    8: ("Arial", 55, "bold"),
+    16: ("Arial", 50, "bold"),
+    32: ("Arial", 50, "bold"),
+    64: ("Arial", 50, "bold"),
+    128: ("Arial", 45, "bold"),
+    256: ("Arial", 45, "bold"),
+    512: ("Arial", 45, "bold"),
+    1024: ("Arial", 40, "bold"),
+    2048: ("Arial", 40, "bold")
+}
